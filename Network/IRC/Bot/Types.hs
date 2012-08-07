e9e8f8e146d3d3c1872d90620f77c8297459e9db
Jonatan Pålsson <jonatan.p@gmail.com>
2012-08-07 15:36:50 +0200

    Linted Types.hs

--- Network/IRC/Bot/Types.hs
+++ Network/IRC/Bot/Types.hs
@@ -1,25 +1,12 @@
-{-# LANGUAGE DeriveDataTypeable, RecordWildCards #-}
+{-# LANGUAGE DeriveDataTypeable #-}
 module Network.IRC.Bot.Types
     ( User(..)
     , nullUser
     ) where
 
-import Control.Concurrent       (ThreadId, forkIO, threadDelay)
-import Control.Concurrent.Chan  (Chan, dupChan, newChan, readChan, writeChan)
-import Control.Concurrent.MVar  (MVar, modifyMVar_, newMVar, readMVar)
-import Control.Concurrent.QSem  (QSem, newQSem, waitQSem, signalQSem)
-import Control.Exception        (IOException, catch)
-import Control.Monad            (mplus, forever, when)
-import Control.Monad.Trans      (liftIO)
 import Data.Data                (Data, Typeable)
-import Data.Time                (UTCTime, addUTCTime, getCurrentTime)
-import Network                  (HostName, PortID(PortNumber), connectTo)
-import Network.IRC              (Message, decode, encode, joinChan, nick, user)
-import Network.IRC              as I
-import Network.IRC.Bot.Log      (Logger, LogLevel(Normal, Debug), stdoutLogger)
-import Network.IRC.Bot.BotMonad (BotMonad(logM), BotPartT, BotEnv(..), runBotPartT)
+import Network                  (HostName)
 import Prelude                  hiding (catch)
-import System.IO                (BufferMode(LineBuffering), Handle, hClose, hGetLine, hPutStrLn, hSetBuffering)
 
 
 data User = User
